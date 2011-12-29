e1bc6d9011366ac1c57573d6745c126ff946e99b
Andriy Kozachuk <dev.oyster@gmail.com>
2011-12-29 20:20:23 +0200

    Cosmetic changes

--- VerifyArgs/Properties/AssemblyInfo.cs
+++ VerifyArgs/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 [assembly: AssemblyDescription("Contains Verify - fast and extensible class for simple method arguments asserts. Example: Verify.NotNull(new { param1, param2 }).")]
 [assembly: AssemblyCompany("Andriy Kozachuk")]
 [assembly: AssemblyProduct("VerifyArgs")]
-[assembly: AssemblyCopyright("Copyright © 2011, Andriy Kozachuk")]
+[assembly: AssemblyCopyright("Copyright (c) 2011, Andriy Kozachuk")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
