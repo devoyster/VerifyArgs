55a81dc9483f28d6cafad1e711a3fa4ab6a3abb5
Andriy Kozachuk <dev.oyster@gmail.com>
2011-12-29 20:21:46 +0200

    ...

--- VerifyArgs/Properties/AssemblyInfo.cs
+++ VerifyArgs/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyTitle("VerifyArgs")]
-[assembly: AssemblyDescription("Contains Verify - fast and extensible class for simple method arguments asserts. Example: Verify.NotNull(new { param1, param2 }).")]
+[assembly: AssemblyDescription("Contains Verify - fast and extensible class for simple method arguments asserts. Example: Verify.Args(new { param1, param2 }).NotNull()")]
 [assembly: AssemblyCompany("Andriy Kozachuk")]
 [assembly: AssemblyProduct("VerifyArgs")]
 [assembly: AssemblyCopyright("Copyright (c) 2011, Andriy Kozachuk")]
