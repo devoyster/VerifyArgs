c042cf6f3e9ac1ef73c2e9245e110343fa3faae2
Andriy Kozachuk <dev.oyster@gmail.com>
2012-01-03 20:27:45 +0200

    Added license text into NuGet binary distribution

--- nuget-publish.cmd
+++ nuget-publish.cmd
@@ -1,4 +1,4 @@
 @cd VerifyArgs
-..\.nuget\nuget.exe pack VerifyArgs.csproj -Prop Configuration=Release
-..\.nuget\nuget.exe push VerifyArgs.1.0.0.0.nupkg -ApiKey
+..\.nuget\nuget.exe pack VerifyArgs.csproj -Version 1.0.0.1 -Prop Configuration=Release -OutputDirectory bin
+..\.nuget\nuget.exe push bin\VerifyArgs.1.0.0.1.nupkg -ApiKey
 @pause
